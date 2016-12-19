66d056a789525decc26eb406f0399864d08bc660
J Harvey <jharvey@cfmaniac.com>
2016-12-19 08:47:13 -0500

    Commit of Length Functions

---
+++ length-utils.js
@@ -0,0 +1,10 @@
+
+//Returns Array Length, like ColdFusion's arraylen() function:
+function arraylen(array) {
+  return array.length;
+}
+
+//Returns String Length, like ColdFusion's len() function:
+function len(string){
+  return string.len;
+}
